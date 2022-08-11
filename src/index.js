b4b44215dfa0d84f9a4430581586ab2bcf288bfe
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-08-11 13:47:40 -0600

    kittens changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 // css stylesheets can be created for each component
 // place them in the src/style directory, and import them like this:
-import "./style/index.css";
+// import "./style/index.css";
 import KittensProvider from "./Providers/KittensProvider";
 
 // ReactDOM.render(<App />, document.getElementById("root"));
