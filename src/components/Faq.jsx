3084b5d3662d22a40de158919a69c7cc96c7ce80
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-08-19 11:36:00 -0600

    faq css

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -5,9 +5,9 @@ export default function Faq() {
     <div class="wrapper border-b-2 mt-9 bg-tan overflow-hidden mx-auto max-w-md rounded shadow-lg">
       Frequently Asked Questions (FAQ)
       <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
-        <div class="w-full lg:w-1/2 px-4 py-2">
-          <details class="mb-4">
-            <summary class="font-semibold bg-red rounded-md py-2 px-4">
+        <div class="w-full px-4 py-2">
+          <details class="mb-4 w-auto">
+            <summary class="font-semibold bg-red rounded-md py-2 px-4 ">
               WHAT DO YOUR ADOPTION FEES INCLUDE?
             </summary>
             <span>
