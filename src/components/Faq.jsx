bd166817796824edabd6601b774c00097063cf25
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-08-19 11:31:15 -0600

    working on FAQ css

--- src/components/Faq.jsx
+++ src/components/Faq.jsx
@@ -2,53 +2,75 @@ import React from "react";
 
 export default function Faq() {
   return (
-    <div>
-      <h1>FAQ</h1>
-      <h3>Frequently Asked Questions:</h3>
+    <div class="wrapper border-b-2 mt-9 bg-tan overflow-hidden mx-auto max-w-md rounded shadow-lg">
       Frequently Asked Questions (FAQ)
-      <h5>WHAT DO YOUR ADOPTION FEES INCLUDE?</h5>
-      <p>
-        The adoption fee covers the animals spay/neuter, current vaccinations,
-        heartworm testing/treatment, feline leukemia and FIV testing, and
-        microchip.
-      </p>
-      <p>
-        Kittens (under 4 months) have been vaccinated against Distemper, Feline
-        Leukemia and Feline AIDS tested, and fecal checked. They have been
-        spayed/neutered. The Rabies Vaccination will be given when the kitten
-        reaches 4 months. A neuter deposit may be necessary when adopting a
-        kitten that is not yet spayed/neutered.
-      </p>
-      <h5>IS IT DIFFICULT TO ADOPT?</h5>
-      <p>
-        We have policies for approving adoptions. They are designed to ensure
-        that each animal is placed with a responsible person prepared to make a
-        lifelong commitment, and to avoid the kinds of problems that may have
-        caused the animal to be brought to the shelter in the first place. An
-        important part of the process is to match the lifestyle and needs of the
-        adopter with the individual dog or cat. If the screening process
-        occasionally seems overly strict, try to remember that the shelter’s
-        first priority is to protect the animal’s best interests.
-      </p>
-      <h5>ARE THERE REQUIREMENTS FOLLOWING THE ADOPTION?</h5>
-      <p>
-        Having a companion animal brings rewards and responsibilities. Following
-        your pet’s adoption, you will be responsible for making sure your pet is
-        safe and for providing regular veterinary care. Be sure your dog has
-        appropriate identification. Provide nutritious food and fresh water for
-        your pet. Make time for exercise, training, and play. Finally, enjoy
-        your new life with your new loving companion.
-      </p>
-      <h5>WHAT IF I HAVE QUESTIONS AFTER I GET MY NEW PET HOME?</h5>
-      <p>
-        We understand that after you get our pet home, the adjustment period can
-        be difficult. We encourage you to call us with any questions or
-        problems, hopefully, before small problems become big ones! We are very
-        experienced with pet transitions and welcome the opportunity to help
-        make it as smooth for you and your pet as possible. And even when all is
-        going well, we love getting calls just to know how our “alumni” are
-        doing.
-      </p>
+      <div class="flex flex-wrap lg:w-4/5 sm:mx-auto sm:mb-2 -mx-2">
+        <div class="w-full lg:w-1/2 px-4 py-2">
+          <details class="mb-4">
+            <summary class="font-semibold bg-red rounded-md py-2 px-4">
+              WHAT DO YOUR ADOPTION FEES INCLUDE?
+            </summary>
+            <span>
+              The adoption fee covers the animals spay/neuter, current
+              vaccinations, heartworm testing/treatment, feline leukemia and FIV
+              testing, and microchip.{" "}
+              <span>
+                {" "}
+                Kittens (under 4 months) have been vaccinated against Distemper,
+                Feline Leukemia and Feline AIDS tested, and fecal checked. They
+                have been spayed/neutered. The Rabies Vaccination will be given
+                when the kitten reaches 4 months. A neuter deposit may be
+                necessary when adopting a kitten that is not yet
+                spayed/neutered.{" "}
+              </span>
+            </span>
+          </details>
+          <details class="mb-4">
+            <summary class="font-semibold bg-red rounded-md py-2 px-4">
+              IS IT DIFFICULT TO ADOPT?
+            </summary>
+            <span>
+              We have policies for approving adoptions. They are designed to
+              ensure that each animal is placed with a responsible person
+              prepared to make a lifelong commitment, and to avoid the kinds of
+              problems that may have caused the animal to be brought to the
+              shelter in the first place. An important part of the process is to
+              match the lifestyle and needs of the adopter with the individual
+              dog or cat. If the screening process occasionally seems overly
+              strict, try to remember that the shelter’s first priority is to
+              protect the animal’s best interests.
+            </span>
+          </details>
+          <details class="mb-4">
+            <summary class="font-semibold bg-red rounded-md py-2 px-4">
+              ARE THERE REQUIREMENTS FOLLOWING THE ADOPTION?
+            </summary>
+            <span>
+              Having a companion animal brings rewards and responsibilities.
+              Following your pet’s adoption, you will be responsible for making
+              sure your pet is safe and for providing regular veterinary care.
+              Be sure your dog has appropriate identification. Provide
+              nutritious food and fresh water for your pet. Make time for
+              exercise, training, and play. Finally, enjoy your new life with
+              your new loving companion.
+            </span>
+          </details>
+          <details class="mb-4">
+            <summary class="font-semibold bg-red rounded-md py-2 px-4">
+              WHAT IF I HAVE QUESTIONS AFTER I GET MY NEW PET HOME?
+            </summary>
+            <span>
+              We understand that after you get our pet home, the adjustment
+              period can be difficult. We encourage you to call us with any
+              questions or problems, hopefully, before small problems become big
+              ones! We are very experienced with pet transitions and welcome the
+              opportunity to help make it as smooth for you and your pet as
+              possible. And even when all is going well, we love getting calls
+              just to know how our “alumni” are doing.
+            </span>
+          </details>
+        </div>
+      </div>
     </div>
   );
 }
