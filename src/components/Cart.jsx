415ebba75dc7bc8f68dba1edc355b689c7665806
chaningan <chaningan@gmail.com>
2022-08-19 13:12:08 -0700

    figuring out totals

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,45 +1,61 @@
 import React, { useEffect, useState } from "react";
 import { useCart } from "../Hooks";
-import { fetchOrdersByCart } from "../api/orders_kitten";
+import { fetchOrdersByCart, fetchOrderById } from "../api/orders_kitten";
+import { fetchKittenById } from "../api/kittens";
+import OrderCard from "./OrderCard";
 
 export default function Cart() {
   const { cart } = useCart();
   const [orders, setOrders] = useState([]);
-  const [subTotal, setSubTotal] = useState([]);
-  console.log("cart from cart", cart);
+  const [subTotal, setSubTotal] = useState(0);
 
   useEffect(() => {
     async function getOrders_kitten(orderId) {
       const fetchedOrders = await fetchOrdersByCart(orderId);
-      console.log("fetchedorders", fetchedOrders);
-      //figuring out this fetchedOrders stuff!!!
 
-      if (fetchedOrders.length) {
-        console.log("setting orders");
+      if (fetchedOrders) {
         setOrders(fetchedOrders);
       }
-      console.log("orders from cart", orders);
     }
     getOrders_kitten(cart.id);
   }, []);
 
-  // useEffect(() => {
-  //   async function getSubTotal(fetchedOrders) {}
-  // });
+  useEffect(() => {
+    async function getSubTotal(fetchedOrders) {
+      // setSubTotal(0);
+      // let priceArray = [];
+      console.log("subtotal1", subTotal);
+      fetchedOrders.forEach(async (order) => {
+        const kitten = await fetchKittenById(order.kitten_id);
+        console.log("kitten in foreach", kitten);
+
+        const newSubtotal = subTotal + kitten.price;
+        console.log("newsub", newSubtotal);
+
+        // priceArray.push(kitten.price);
+        console.log("subtotal", subTotal);
+        // return priceArray;
+        setSubTotal(newSubtotal);
+      });
+    }
+    getSubTotal(orders);
+    // console.log("subtatol array", subTotalArray);
+  }, [orders]);
+  console.log("final subtotal", subTotal);
 
   return (
     <div>
       <h1>Cart</h1>
       <div className="orderSummary">
-        {orders}
-        {/* {orders.map((order, index) => {
-          return <OrderCard key={`${order.id}`} order={order} />;
-        })} */}
+        {orders.map((order) => {
+          return <OrderCard key={`Key:${order.id}`} order={order} />;
+        })}
       </div>
 
       <div>
-        Subtotal: {`${subTotal}`}
-        Tax: Total:
+        <p> Subtotal: {`${subTotal}`}</p>
+        <p>Tax:</p>
+        <p>Total:</p>
       </div>
     </div>
   );
--- src/components/OrderCard.jsx
+++ src/components/OrderCard.jsx
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
-import fetchKittenById from "../api/kittens";
+import { fetchKittenById } from "../api/kittens";
 
-const OrderCard = ({ order_id, kitten_id }) => {
+const OrderCard = (order) => {
   const [kitten, setKitten] = useState({});
-
+  const kitten_id = order.order.kitten_id;
+  // console.log("order in ordercard", order);
+  // console.log("kittenid", kitten_id);
   useEffect(() => {
     async function getKitten(kittenId) {
       const fetchedKitten = await fetchKittenById(kittenId);
@@ -14,8 +16,10 @@ const OrderCard = ({ order_id, kitten_id }) => {
 
   return (
     <div>
-      <p>${kitten.name}</p>
-      <p>${kitten.description}</p>
+      <img src={kitten.img_url} />
+      <p>{kitten.name}</p>
+      <p>{kitten.description}</p>
+      <p>${kitten.price}.00</p>
     </div>
   );
 };
