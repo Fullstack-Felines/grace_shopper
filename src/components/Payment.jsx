7ddded55ac89e12a9d82e093b3ea2281dd248898
SMan91 <stephan.manley@yahoo.com>
2022-08-22 15:19:20 -0600

    almost got checkout working

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,38 +1,53 @@
 import React, { useState } from "react";
 import { useCart } from "../Hooks";
-import { updateCart } from "../api/orders";
+import { updateCart, createCart } from "../api/orders";
 import { updateKitten } from "../api/kittens";
+import { useNavigate } from "react-router-dom";
 
 export default function Payment() {
   const { cart, setCart } = useCart();
   const [shippingAddress, setShippingAddress] = useState("");
+  const navigate = useNavigate();
 
   return (
     <div>
       <form
         onSubmit={async () => {
-          await updateCart(cart.id, {
+          //update cart to be inactive and update shipping address
+
+          const newCart = await createCart({
             customer_id: cart.customer_id,
-            total_amount: cart.total_amount,
+            total_amount: 0,
+            is_active: true,
+            shipping_address: shippingAddress,
+          });
+
+          const oldCart = cart;
+          setCart(newCart);
+
+          await updateCart(oldCart.id, {
+            customer_id: oldCart.customer_id,
+            total_amount: oldCart.total_amount,
             is_active: false,
             shipping_address: shippingAddress,
           });
 
-          //update cart to be inactive and update shipping address
           //update kittens to be unavailable
           cart.orders_kitten.forEach(async (order) => {
-            const kitten = await fetchKittenById(order.kitten_id);
-            await updateKitten(order.kitten_id, {
-              name: kitten.name,
-              breed: kitten.breed,
-              description: kitten.description,
-              price: kitten.price,
-              img_url: kitten.img_url,
+            await updateKitten({
+              kittenId: order.kittens.id,
+              name: order.kittens.name,
+              breed: order.kittens.breed,
+              description: order.kittens.description,
+              price: order.kittens.price,
+              img_url: order.kittens.img_url,
               available: false,
             });
           });
+          // START HERE TOMORROW (Hitting everything except the two things below);
           alert("Thank you for your purchase!");
           //navigate to home page
+          navigate("/");
         }}
       >
         <input placeholder="Name on Card" />
