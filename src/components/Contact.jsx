235a836c3181345f7c6422bd44969262e2126808
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-08-23 14:57:09 -0600

    faq css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,13 +7,13 @@ export default function Contact() {
         <div class="flex flex-wrap -mx-3 mb-6 ">
           <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
             <label
-              class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+              class="block uppercase tracking-wide text-coral text-xs font-bold mb-2"
               for="grid-first-name"
             >
               First Name
             </label>
             <input
-              class="appearance-none block w-full bg-red text-cafe border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              class="appearance-none block w-full bg-red text-coral border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               id="grid-first-name"
               type="text"
               placeholder=""
@@ -21,13 +21,13 @@ export default function Contact() {
           </div>
           <div class="w-full md:w-1/2 px-3">
             <label
-              class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+              class="block uppercase tracking-wide text-coral text-xs font-bold mb-2"
               for="grid-last-name"
             >
               Last Name
             </label>
             <input
-              class="appearance-none block w-full bg-red text-cafe border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              class="appearance-none block w-full bg-red text-coral border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="grid-last-name"
               type="text"
               placeholder=""
@@ -37,13 +37,13 @@ export default function Contact() {
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+              class="block uppercase tracking-wide text-coral text-xs font-bold mb-2"
               for="grid-password"
             >
               E-mail
             </label>
             <input
-              class="appearance-none block w-full bg-red text-cafe border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              class="appearance-none block w-full bg-red text-coral border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
               id="email"
               type="email"
             />
@@ -52,13 +52,13 @@ export default function Contact() {
         <div class="flex flex-wrap -mx-3 mb-6">
           <div class="w-full px-3">
             <label
-              class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+              class="block uppercase tracking-wide text-coral text-xs font-bold mb-2"
               for="grid-password"
             >
               Message
             </label>
             <textarea
-              class=" no-resize appearance-none block w-full bg-red text-cafe border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              class=" no-resize appearance-none block w-full bg-red text-coral border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
               id="message"
             ></textarea>
           </div>
@@ -67,7 +67,7 @@ export default function Contact() {
           <div class="md:w-1/3">
             <button
               onClick={() => alert("Message Sent!")}
-              class="shadow bg-cafe hover:bg-red focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              class="shadow bg-red hover:bg-coral  focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
               type="submit"
             >
               Send
