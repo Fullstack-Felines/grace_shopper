ed0e39418782bb7af642cd86e2d359b8fb20f637
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-08-23 10:41:44 -0600

    contact us css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,78 +2,80 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <form class="w-full max-w-lg p-3  items-center">
-      <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          <label
-            class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
-            for="grid-first-name"
-          >
-            First Name
-          </label>
-          <input
-            class="appearance-none block w-full bg-gray-200 text-cafe border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
-            id="grid-first-name"
-            type="text"
-            placeholder=""
-          />
+    <div class=" flex flex-row  justify-center items-center">
+      <form class="w-full max-w-lg p-3">
+        <div class="flex flex-wrap -mx-3 mb-6 ">
+          <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+            <label
+              class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+              for="grid-first-name"
+            >
+              First Name
+            </label>
+            <input
+              class="appearance-none block w-full bg-red text-cafe border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+              id="grid-first-name"
+              type="text"
+              placeholder=""
+            />
+          </div>
+          <div class="w-full md:w-1/2 px-3">
+            <label
+              class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+              for="grid-last-name"
+            >
+              Last Name
+            </label>
+            <input
+              class="appearance-none block w-full bg-red text-cafe border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="grid-last-name"
+              type="text"
+              placeholder=""
+            />
+          </div>
         </div>
-        <div class="w-full md:w-1/2 px-3">
-          <label
-            class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
-            for="grid-last-name"
-          >
-            Last Name
-          </label>
-          <input
-            class="appearance-none block w-full bg-gray-200 text-cafe border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="grid-last-name"
-            type="text"
-            placeholder=""
-          />
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+              for="grid-password"
+            >
+              E-mail
+            </label>
+            <input
+              class="appearance-none block w-full bg-red text-cafe border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              id="email"
+              type="email"
+            />
+          </div>
         </div>
-      </div>
-      <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full px-3">
-          <label
-            class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
-            for="grid-password"
-          >
-            E-mail
-          </label>
-          <input
-            class="appearance-none block w-full bg-gray-200 text-cafe border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-            id="email"
-            type="email"
-          />
+        <div class="flex flex-wrap -mx-3 mb-6">
+          <div class="w-full px-3">
+            <label
+              class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+              for="grid-password"
+            >
+              Message
+            </label>
+            <textarea
+              class=" no-resize appearance-none block w-full bg-red text-cafe border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+              id="message"
+            ></textarea>
+          </div>
         </div>
-      </div>
-      <div class="flex flex-wrap -mx-3 mb-6">
-        <div class="w-full px-3">
-          <label
-            class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
-            for="grid-password"
-          >
-            Message
-          </label>
-          <textarea
-            class=" no-resize appearance-none block w-full bg-gray-200 text-cafe border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
-            id="message"
-          ></textarea>
+        <div class="md:flex md:items-center">
+          <div class="md:w-1/3">
+            <button
+              onClick={() => alert("Message Sent!")}
+              class="shadow bg-cafe hover:bg-red focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+              type="submit"
+            >
+              Send
+            </button>
+          </div>
+          <div class="md:w-2/3"></div>
         </div>
-      </div>
-      <div class="md:flex md:items-center">
-        <div class="md:w-1/3">
-          <button
-            onClick={() => alert("Message Sent!")}
-            class="shadow bg-cafe hover:bg-red focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
-            type="submit"
-          >
-            Send
-          </button>
-        </div>
-        <div class="md:w-2/3"></div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
