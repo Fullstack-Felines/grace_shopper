9081a641a795b1253d8331b9a9fd4b6230d12ab6
Jaydenlundahl <jaydenlundahl20@icloud.com>
2022-08-23 10:33:28 -0600

    contact us css

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,31 +2,78 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <div>
-      <section class="contact container" id="contact">
-        <h2 class="heading">Contact Us!</h2>
-        <form action="" class="contact-form" id="contact-form">
-          <input type="text" placeholder="Your Name" class="name" required />
+    <form class="w-full max-w-lg p-3  items-center">
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+          <label
+            class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+            for="grid-first-name"
+          >
+            First Name
+          </label>
           <input
-            type="email"
-            name=""
-            id=""
-            placeholder="Email Address"
-            class="email"
-            required
+            class="appearance-none block w-full bg-gray-200 text-cafe border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+            id="grid-first-name"
+            type="text"
+            placeholder=""
           />
+        </div>
+        <div class="w-full md:w-1/2 px-3">
+          <label
+            class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+            for="grid-last-name"
+          >
+            Last Name
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-cafe border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="grid-last-name"
+            type="text"
+            placeholder=""
+          />
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+            for="grid-password"
+          >
+            E-mail
+          </label>
+          <input
+            class="appearance-none block w-full bg-gray-200 text-cafe border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+            id="email"
+            type="email"
+          />
+        </div>
+      </div>
+      <div class="flex flex-wrap -mx-3 mb-6">
+        <div class="w-full px-3">
+          <label
+            class="block uppercase tracking-wide text-cafe text-xs font-bold mb-2"
+            for="grid-password"
+          >
+            Message
+          </label>
           <textarea
-            name=""
-            id=""
-            cols="30"
-            rows="10"
-            placeholder="Write message here"
-            class="message"
-            required
+            class=" no-resize appearance-none block w-full bg-gray-200 text-cafe border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none"
+            id="message"
           ></textarea>
-          <input type="submit" value="Submit" class="send-btn" />
-        </form>
-      </section>
-    </div>
+        </div>
+      </div>
+      <div class="md:flex md:items-center">
+        <div class="md:w-1/3">
+          <button
+            onClick={() => alert("Message Sent!")}
+            class="shadow bg-cafe hover:bg-red focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded"
+            type="submit"
+          >
+            Send
+          </button>
+        </div>
+        <div class="md:w-2/3"></div>
+      </div>
+    </form>
   );
 }
