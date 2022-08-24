6e73fd567ec569518a12de84a894701f636a5bc5
nicofrancis <nicolofrancis10@gmail.com>
2022-08-24 13:30:58 -0600

    responsive nav

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,58 +11,61 @@ export default function NavBar() {
 
   return (
     <div class="bg-pink px-2 sm:px-4 py-2.5 rounded min-h-80">
-      <nav class="flex flex-wrap justify-evenly items-center mx-auto">
+      <nav class="flex flex-wrap justify-evenly flex-col sm:flex-row items-center mx-auto">
         <Link to="/" class="flex items-center">
           <img
             src="https://res.cloudinary.com/dkmoq49jf/image/upload/v1661282043/Fullstack_Felines_1_o93p2j.png"
-            class="object-contain h-14 transition duration-300 hover:scale-125"
+            class="object-contain h-25 sm:h-14 transition duration-300 hover:scale-125"
           />
         </Link>
-        <Link
-          to="/"
-          class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 "
-        >
-          Home
-        </Link>
-        <Link
-          to="/AboutUs"
-          class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 "
-        >
-          About
-        </Link>
-        <Link
-          to="/Kittens"
-          class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 "
-        >
-          Kittens
-        </Link>
-        <Link
-          to="/Cart"
-          class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 "
-        >
-          Cart
-        </Link>
-
-        {user.username ? (
-          <button
-            onClick={async () => {
-              const result = await logoutUser();
-              setUser({ loggedIn: false });
-
-              // setCart(guestCart);
-              console.log("User state after logout:", user);
-            }}
-          >
-            Logout
-          </button>
-        ) : (
+        <div class="flex text-center">
           <Link
-            to="/Login"
-            class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 "
+            to="/"
+            class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 mx-2"
           >
-            Login
+            Home
           </Link>
-        )}
+          <Link
+            to="/AboutUs"
+            class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 mx-2"
+          >
+            About
+          </Link>
+          <Link
+            to="/Kittens"
+            class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 mx-2"
+          >
+            Kittens
+          </Link>
+          <Link
+            to="/Cart"
+            class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 mx-2"
+          >
+            Cart
+          </Link>
+
+          {user.username ? (
+            <button
+              class="text-coral text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 mx-2"
+              onClick={async () => {
+                const result = await logoutUser();
+                setUser({ loggedIn: false });
+
+                // setCart(guestCart);
+                console.log("User state after logout:", user);
+              }}
+            >
+              Logout
+            </button>
+          ) : (
+            <Link
+              to="/Login"
+              class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 mx-2"
+            >
+              Login
+            </Link>
+          )}
+        </div>
       </nav>
     </div>
   );
