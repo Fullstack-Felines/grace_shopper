f2b767e34398de99b0486dc24422d2770740ee30
nicofrancis <nicolofrancis10@gmail.com>
2022-08-23 21:37:36 -0600

    cart magic

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -20,44 +20,100 @@ export default function Cart() {
   }
 
   return (
-    <div class="max-w-md mx-auto bg-gray-100 shadow-lg rounded-lg  md:max-w-5xl">
-      <div class="md:flex ">
-        <div class="w-full p-4 px-5 py-5">
-          <div class="md:grid md:grid-cols-3 gap-2 ">
-            <div class="col-span-2 p-5">
-              <h1>Cart</h1>
-              {cart.orders_kitten ? (
-                <div>
-                  <div className="orderSummary">
-                    {cart.orders_kitten.map((order) => {
-                      return (
-                        <OrderCard key={`Key:${order.id}`} order={order} />
-                      );
-                    })}
-                  </div>
-
+    <div class="bg-gradient-to-b from-pink to-cultured">
+      <div class="max-w-md mx-auto bg-white shadow-lg rounded-lg  md:max-w-5xl">
+        <div class="md:flex ">
+          <div class="w-full p-4 px-5 py-5">
+            <div class="md:grid md:grid-cols-3 gap-2 ">
+              <div class="col-span-2 p-5">
+                <h1>Cart</h1>
+                {cart.orders_kitten ? (
                   <div>
-                    <p> Subtotal: ${calcCartTotal(cart)}.00</p>
+                    <div className="orderSummary">
+                      {cart.orders_kitten.map((order) => {
+                        return (
+                          <OrderCard key={`Key:${order.id}`} order={order} />
+                        );
+                      })}
+                    </div>
 
-                    <p>
-                      Tax: $
-                      {(calcCartTotal(cart) * Math.round(0.07 * 1000)) / 1000}
-                    </p>
-                    <p>
-                      Total: $
-                      {calcCartTotal(cart) +
-                        (calcCartTotal(cart) * Math.round(0.07 * 1000)) / 1000}
-                    </p>
-                    <button
-                      onClick={() => {
-                        navigate("/Payment");
-                      }}
-                    >
-                      Checkout
-                    </button>
+                    <div>
+                      <p> Subtotal: ${calcCartTotal(cart)}.00</p>
+
+                      <p>
+                        Tax: $
+                        {(calcCartTotal(cart) * Math.round(0.07 * 1000)) / 1000}
+                      </p>
+                      <p>
+                        Total: $
+                        {calcCartTotal(cart) +
+                          (calcCartTotal(cart) * Math.round(0.07 * 1000)) /
+                            1000}
+                      </p>
+
+                      <div class="flex justify-center flex-col pt-3">
+                        <label class="text-xs text-gray-400 ">
+                          Name on Card
+                        </label>
+                        <input
+                          type="text"
+                          class="focus:outline-none w-full h-6 bg-gray-800 text-black placeholder-gray text-sm border-b border-gray-600 py-4"
+                          placeholder="Name"
+                        ></input>
+                      </div>
+
+                      <div class="flex justify-center flex-col pt-3">
+                        <label class="text-xs text-gray-400 ">
+                          Card Number
+                        </label>
+                        <input
+                          type="text"
+                          class="focus:outline-none w-full h-6 bg-white text-black placeholder-grey text-sm border-b border-gray-600 py-4"
+                          placeholder="****     ****      ****      ****"
+                        ></input>
+                      </div>
+
+                      <div class="grid grid-cols-3 gap-2 pt-2 mb-3">
+                        <div class="col-span-2 ">
+                          <label class="text-xs text-gray-400">
+                            Expiration Date
+                          </label>
+                          <div class="grid grid-cols-2 gap-2">
+                            <input
+                              type="text"
+                              class="focus:outline-none w-full h-6 bg-white text-black placeholder-gray text-sm border-b border-gray py-4"
+                              placeholder="mm"
+                            ></input>
+                            <input
+                              type="text"
+                              class="focus:outline-none w-full h-6 bg-white text-black placeholder-gray text-sm border-b border-gray py-4"
+                              placeholder="yyyy"
+                            ></input>
+                          </div>
+                        </div>
+
+                        <div class="">
+                          <label class="text-xs text-gray-400">CVV</label>
+                          <input
+                            type="text"
+                            class="focus:outline-none w-full h-6 bg-gray-800 text-white placeholder-gray-300 text-sm border-b border-gray-600 py-4"
+                            placeholder="XXX"
+                          ></input>
+                        </div>
+                      </div>
+
+                      <button
+                        class="h-12 w-full bg-blue-500 rounded focus:outline-none text-white hover:bg-blue-600"
+                        onClick={() => {
+                          navigate("/Payment");
+                        }}
+                      >
+                        Checkout
+                      </button>
+                    </div>
                   </div>
-                </div>
-              ) : null}
+                ) : null}
+              </div>
             </div>
           </div>
         </div>
