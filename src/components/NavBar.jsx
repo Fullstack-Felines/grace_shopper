443e24b913a75f3301aad314815f7625d4960e90
nicofrancis <nicolofrancis10@gmail.com>
2022-08-24 19:37:40 -0600

    about updates

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -8,18 +8,18 @@ export default function NavBar() {
   const { user, setUser } = useAuth();
 
   return (
-    <div class="bg-pink px-2 sm:px-4 py-2.5 rounded min-h-80">
-      <nav class="flex justify-evenly flex-col sm:flex-row sm:justify-evenly items-center mx-auto">
-        <Link to="/" class="flex ">
+    <div class="bg-pink px-2 sm:px-4 py-2.5 rounded min-h-80 text-center">
+      <nav class="sm:flex sm:flex-row sm:justify-evenly items-center mx-auto ">
+        <Link to="/" class="flex justify-center">
           <img
             src="https://res.cloudinary.com/dkmoq49jf/image/upload/v1661282043/Fullstack_Felines_1_o93p2j.png"
-            class="object-contain h-25 sm:h-14 xl:h-20 transition duration-300 hover:scale-125 sm:flex-1 sm:flex-row sm:ml-5 sm:justify-start m-2"
+            class="object-contain h-25 sm:h-16 xl:h-20 transition duration-300 hover:scale-125 sm:flex-1 sm:flex-row sm:ml-5 sm:justify-start m-2 "
           />
         </Link>
         {/* <div class="flex text-center sm:justify-center  sm:flex-row sm:w-1/3"> */}
         <Link
           to="/"
-          class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 mx-2"
+          class="text-coral  text-xl font-bold transform  hover:text-white transition duration-300 hover:scale-125 mx-2 inline-block"
         >
           Home
         </Link>
