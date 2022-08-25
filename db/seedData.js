78f0d8ad2298482b80e7a9131402b9a5496fd67a
nicofrancis <nicolofrancis10@gmail.com>
2022-08-25 10:01:00 -0600

    final finallll push

--- db/seedData.js
+++ db/seedData.js
@@ -6,7 +6,7 @@ const kittens = [
       "I love to be around people and play with toys and other cats. I’m also great with kids!",
     price: 100,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660672899/pexels-alex-bargain-1472999_dvhhtl.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661269947/BuJHgqzHm4I-png__700_epuwzp.jpg",
     available: true,
   },
   {
@@ -16,7 +16,7 @@ const kittens = [
       "If you’ve ever wanted a fluffy, sweet little friend who will make your day with adorable antics and the cutest cuddling, I’m here to make all your dreams come true!",
     price: 20,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660797843/My_project_2_xu2uld.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661436389/GettyImages-540542926-scaled-e1619016093503_h8ut1x.jpg",
     available: true,
   },
   {
@@ -26,7 +26,7 @@ const kittens = [
       "I have such a sweet little spirit about me and I know in a patient home, I will just be the most amazing companion!",
     price: 80,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660800042/My_project_10_ei5b1p.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661436510/sergey-semin-Y0WXj3xqJz0-unsplash_siku5d.jpg",
     available: false,
   },
   {
@@ -36,7 +36,7 @@ const kittens = [
       "I’m a little timid right now and I’m not ready to seek out affection from humans quite yet, but I’m having so much fun learning all about the world and how to be a cat.",
     price: 160,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660672812/My_project_1_aq61ly.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661436676/the-lucky-neko-QlF3uMnPZnw-unsplash_gypb1g.jpg",
     available: true,
   },
   {
@@ -46,7 +46,7 @@ const kittens = [
       "I love to run and play with my siblings! I am especially good at chasing my toy mouse and leaping up into the air once I capture it!",
     price: 1600,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660798114/My_project_2_tozhrk.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661437681/sincerely-media-wz2aU5cF3zQ-unsplash_adobe_express_fpizva.jpg",
     available: true,
   },
   {
@@ -56,7 +56,7 @@ const kittens = [
       "I haven't had a lot of experience being around humans so I'm initially a little bit shy around new people. But I'm very sweet and receptive if you approach me slowly!",
     price: 80,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660797968/My_project_2_jeo58c.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661437818/kym-ellis-yDfxzqUXFdI-unsplash_adobe_express_yhebbd.jpg",
     available: true,
   },
   {
@@ -66,7 +66,7 @@ const kittens = [
       "I am an expert at climbing my scratching posts and I win the race to the top against my sister Peppa every time!",
     price: 100,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660798359/My_project_4_iddqeb.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661437011/remi-remino-E9kVmtiqqGE-unsplash_2_jnxxxp.jpg",
     available: false,
   },
   {
@@ -76,7 +76,7 @@ const kittens = [
       "I am a very sweet girl and I love ear scratches and chin rubs! I’m ready to play with you!",
     price: 20,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660798228/My_project_3_uvzzmo.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661434289/the-lucky-neko-55ClwmQyNiU-unsplash_t3302c.jpg",
     available: true,
   },
   {
@@ -86,7 +86,7 @@ const kittens = [
       "I am a fun and playful little guy with lots of energy, but I can also nap like a champ in your lap! ",
     price: 250,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660798476/My_project_5_kpy6hu.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661437349/the-lucky-neko-w2ii07oH92s-unsplash_adtib3.jpg",
     available: true,
   },
   {
@@ -96,7 +96,7 @@ const kittens = [
       "I use the litter box, and scratch posts like a big boy, and my foster mom says I’m a good eater! I look forward to meeting you!",
     price: 2500,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660798709/My_project_7_d9ratz.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661270062/pexels-pixabay-45201_1_yq7opo.jpg",
     available: false,
   },
   {
@@ -106,7 +106,7 @@ const kittens = [
       "Right now I am full of energy and very curious. I meet guests at the door and love nothing more than to run and chase my best friend and fostermate, Zeus.",
     price: 3000,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660798632/My_project_6_nrqtvs.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661437252/jeanie-de-klerk-QfOnteIcGZU-unsplash_adobe_express_dwfxau.jpg",
     available: true,
   },
   {
@@ -116,7 +116,7 @@ const kittens = [
       " My foster says I have no bad habits, am litterbox trained and that I make her laugh all day.",
     price: 80,
     img_url:
-      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1660672611/My_project_v3xnoy.jpg",
+      "https://res.cloudinary.com/dkmoq49jf/image/upload/v1661437490/nihal-karkala-nynzG3yNG34-unsplash_adobe_express_zomeqg.jpg",
     available: true,
   },
 ];
